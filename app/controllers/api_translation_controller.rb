7b2239536fb032ae1b7510371119bc5184a88435
radekstasiak <radek.stasiak@gmail.com>
2016-11-22 11:56:37 +0000

    Parse google translate response to json format

--- app/controllers/api_translation_controller.rb
+++ app/controllers/api_translation_controller.rb
@@ -1,17 +1,27 @@
 require 'rest-client'
+require 'json'
 
 class ApiTranslationController < BaseApiController
-
+	
 	def full_translation
 		@word = params[:word]
 		@sourceLang = params[:sl]
 		@translateLang = params[:tl]
-		puts(@type)
-		puts(@word)
-		puts(@sourceLang)
-		puts(@translateLang)
+
 		@response=RestClient.post('https://translate.googleapis.com/translate_a/single', {'client'=>'gtx','sl'=>@sourceLang,'tl'=>@translateLang,'dt'=>'t','q'=>@word})
-		render :json => {:type => @response.to_json}
+		puts(@response)
+		@result=@response.body
+		puts(@result)
+		while @result.include?(',,')
+			puts(@result)
+			@result.gsub!(',,',',"",')
+		end
+
+		@jsonArray = JSON.parse(@result)
+		render :json => {:translatedText => @jsonArray[0][0][0],
+						 :originalText => @jsonArray[0][0][1],
+						 :sourceLang => @jsonArray[2]}
+
 	end
 
 end
