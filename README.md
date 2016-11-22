45b54a2ef8505ddea3119ef5680832c9dc486fe9
radekstasiak <radek.stasiak@gmail.com>
2016-11-22 13:16:33 +0000

    Refactor code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # memorizer-web
+
+to get rest client output
+export RESTCLIENT_LOG='stdout'
--- app/controllers/api_translation_controller.rb
+++ app/controllers/api_translation_controller.rb
@@ -2,25 +2,42 @@ require 'rest-client'
 require 'json'
 
 class ApiTranslationController < BaseApiController
-	
+
 	def full_translation
+
 		@word = params[:word]
 		@sourceLang = params[:sl]
 		@translateLang = params[:tl]
+		RestClient.log = 'stdout'
+		@test='at?dt=t'
+		@response=RestClient.post('https://translate.googleapis.com/translate_a/single', 
+								  {'client'=>'gtx',
+								  'sl'=>@sourceLang,
+								  'tl'=>@translateLang,
+								  'dt'=>@test,
+								  'q'=>@word}){|response, request, result, &block |
 
-		@response=RestClient.post('https://translate.googleapis.com/translate_a/single', {'client'=>'gtx','sl'=>@sourceLang,'tl'=>@translateLang,'dt'=>'t','q'=>@word})
-		puts(@response)
-		@result=@response.body
-		puts(@result)
-		while @result.include?(',,')
-			puts(@result)
-			@result.gsub!(',,',',"",')
-		end
+				case response.code
+  					when 200
+    				@jsonArray=response.body
+    					while @jsonArray.include?(',,') or @jsonArray.include?('[,') or @jsonArray.include?(',]')
+							puts(@jsonArray)
+							@jsonArray.gsub!(',,',',"",')
+							@jsonArray.gsub!(',]',',""]')
+							@jsonArray.gsub!('[,','["",')
+						end
 
-		@jsonArray = JSON.parse(@result)
-		render :json => {:translatedText => @jsonArray[0][0][0],
-						 :originalText => @jsonArray[0][0][1],
-						 :sourceLang => @jsonArray[2]}
+					@result = JSON.parse(@jsonArray)
+  						else
+  						@result=response.code
+    					end
+		}
+
+		
+		render :json => {:type => @result}
+		#render :json => {:translatedText => @jsonArray[0][0][0],
+		#				 :originalText => @jsonArray[0][0][1],
+		#				 :sourceLang => @jsonArray[2]}
 
 	end
 
