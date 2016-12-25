192e5d8a9ee8451a15b83ec1f6b9130a3333271f
Radek <radek.stasiak@gmail.com>
2016-12-25 14:23:13 +0100

    Add more email validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}
 	attr_accessor :name, :email
 end
