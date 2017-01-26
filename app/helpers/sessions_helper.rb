7122b8f5f75545c566d0e6939e03a5c466e64481
radekstasiak <radek.stasiak@gmail.com>
2017-01-26 16:21:47 +0000

    Add changes not commited with previous push

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		# if @current_user exists then use it, if its nil then lookup in db
+		# ||= is the same as @current_user= @current_user || User.find_by(id: session[:user_id])
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+    	!current_user.nil?
+  	end
 end
