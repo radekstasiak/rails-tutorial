4e4b208f8ec79f5e5349ccc009d49b14c7c582f7
radekstasiak <radek.stasiak@gmail.com>
2017-03-01 12:53:29 +0000

    Implement password reset mechanism with edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,10 @@ def User.digest(string)
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   
   private
 
